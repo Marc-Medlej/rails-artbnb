fdec4de9519386b14e8826a8ceecb31f2909eb86
Marc Medlej <medlejmarc@gmail.com>
2020-08-17 17:48:23 +0100

    Added first_name and last_name, split routes for visitors and users, added navbar

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,28 +1,28 @@
 class ArtworksController < ApplicationController
-    def index
-      @artworks = Artwork.all
-    end
+  skip_before_action :authenticate_user!, only: [ :index, :show ]
 
-    def show
-      @artwork = Artwork.find(params[:id])
-    end
+  def index
+    @artworks = Artwork.all
+  end
 
-    def new
-      @artwork = Artwork.new
-    end
+  def show
+    @artwork = Artwork.find(params[:id])
+  end
 
-    def create
-      @artwork = Artwork.new(artwork_params)
-      @artwork.user = current_user
-      @artwork.save
+  def new
+    @artwork = Artwork.new
+  end
 
-      # no need for app/views/artworks/create.html.erb
-      redirect_to artwork_path(@artwork)
-    end
+  def create
+    @artwork = Artwork.new(artwork_params)
+    @artwork.user = current_user
+    @artwork.save
+    redirect_to artwork_path(@artwork)
+  end
 
-    private
+  private
 
-    def artwork_params
-      params.require(:artwork).permit(:name, :artist_name, :location, :description, :price_per_day)
-    end
+  def artwork_params
+    params.require(:artwork).permit(:name, :artist_name, :location, :description, :price_per_day)
+  end
 end
