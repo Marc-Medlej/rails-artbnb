1fa45bf9a0188ecaed4503545cff52c51aa03d97
Marc Medlej <medlejmarc@gmail.com>
2020-08-18 14:50:07 +0100

    Accept / Decline a booking

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   resources :artworks, only: [ :index, :show, :new, :create ] do
     resources :bookings, only: [ :new, :create ]
   end
-  resources :bookings, only: [ :edit, :update ]
+  resources :bookings, only: [  :update ]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
