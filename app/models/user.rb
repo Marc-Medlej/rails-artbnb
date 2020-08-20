d66bcb13a1ee1b5a8a0e4bac803db62bea5bd6ee
Simon Walton <saw29@hotmail.co.uk>
2020-08-20 13:37:30 +0100

    profile page drop tabs still need work to fix div. also updated the user model to be able to access the bookings through art

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-
+  has_many :artworks
+  has_many :bookings
+  has_many :artwork_bookings, through: :artworks, source: :bookings
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
